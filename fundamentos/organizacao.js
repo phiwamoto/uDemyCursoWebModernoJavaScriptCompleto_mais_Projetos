393440bdc81191e36566fee818016124199f9bf6
unknown <phiwamoto@hotmail.com>
2021-05-16 10:50:26 -0300

    Fundamentos ate aula 34

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,9 @@
+console.log(
+    "Sentença de Código") // Pode ou não conter o ";" ponto-virgula no final" 
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!') // Padrão do curso   
+    }
+}
