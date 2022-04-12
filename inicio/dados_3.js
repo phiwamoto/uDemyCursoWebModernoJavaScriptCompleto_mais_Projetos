7046b104ed03a857dfc890adad42b5968f5e7ea1
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2022-04-11 21:29:59 -0300

    Retomada a aula de Javascript

---
+++ inicio/dados_3.js
@@ -0,0 +1,14 @@
+// 3 tipos básicos suportados no Javascipt (existem mais, mas esses são extremamente importante)
+
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
---
+++ inicio/dados_4.js
@@ -0,0 +1,9 @@
+const a = 3;
+
+// ...
+
+// a = a + 10;  // TypeError: Assignment to constant variable. ---> Erro ao tentar alterar a constante
+
+// ...
+
+console.log(a);
